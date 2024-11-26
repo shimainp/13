63866a2e35e6ff77db705c12ff7f82515abdedfc
shimainp <157105593+shimainp@users.noreply.github.com>
2024-11-26 17:49:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ client.once(Events.ClientReady, async readyClient => {
 					}
 				} else {
 					//console.log(`Today is ${currentDayOfWeek} and the time is ${currentTime}`);
-					if (currentDayOfWeek == "Tuesday") {
+					if (currentDayOfWeek == "Sunday") {
 						if (ae === undefined) {
 							channel.send({ content: '@everyone', embeds: [exampleEmbed], components: [row], fetchReply: true });
 						} else {
